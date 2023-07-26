bf6202e5b4f1a3f3d80508d513e75e5dc90b563b
Oyaps <oyapiasamoah95@gmail.com>
2023-07-26 13:41:05 -0500

    adding /visitors to fetch line

--- resume-frontend/apifetch.js
+++ resume-frontend/apifetch.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById("visitors");
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod')
+    fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
         .then(response => {
         return response.json()
       })
