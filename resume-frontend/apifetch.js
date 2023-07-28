03f465bfdcacd4022455c12e26b806bab824792b
Oyaps <oyapiasamoah95@gmail.com>
2023-07-28 11:26:21 -0500

    api

--- resume-frontend/apifetch.js
+++ resume-frontend/apifetch.js
@@ -1,14 +1,29 @@
+// const countEl = document.getElementById("visitors");
+
+// updateVisitCount();
+
+// function updateVisitCount() {
+//   fetch("https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       document.getElementById("visitors").innerHTML = data["count"];
+//     });
+// }
+
 const countEl = document.getElementById("visitors");
 
 updateVisitCount();
 
 function updateVisitCount() {
-  fetch("https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors")
+  fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      document.getElementById("visitors").innerHTML = data["count"];
+      document.getElementById("visitors").innerHTML = data['count'];
     });
 }
