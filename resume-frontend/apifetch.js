66a249aea44ad48a1e6c2d2ec9041d9011b39671
Oyaps <oyapiasamoah95@gmail.com>
2023-07-28 10:29:22 -0500

    api reversal

--- resume-frontend/apifetch.js
+++ resume-frontend/apifetch.js
@@ -3,8 +3,7 @@ const countEl = document.getElementById("visitors");
 updateVisitCount();
 
 function updateVisitCount() {
-  fetch(" https://z0bf3i4492.execute-api.us-east-1.amazonaws.com/Prod/visits")
-    // fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
+  fetch("https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors")
     .then((response) => {
       return response.json();
     })
