2b16e875481a130fa4fa14d568af458e77d4b434
Oyaps <oyapiasamoah95@gmail.com>
2023-07-28 10:18:52 -0500

    api change

--- resume-frontend/apifetch.js
+++ resume-frontend/apifetch.js
@@ -3,12 +3,13 @@ const countEl = document.getElementById("visitors");
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
-        .then(response => {
-        return response.json()
-      })
-        .then(data => {
-            console.log(data);
-            document.getElementById("visitors").innerHTML = data['count'];
-        })
-    }
+  fetch(" https://z0bf3i4492.execute-api.us-east-1.amazonaws.com/Prod/visits")
+    // fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById("visitors").innerHTML = data["count"];
+    });
+}
