28988fbd5d6ccc314e74ba31a7599bd8001fc215
Oyaps <oyapiasamoah95@gmail.com>
2023-08-13 18:01:29 -0500

    new api gateway

--- resume-backend/modules/lambda_api_module/apigw.tf
+++ resume-backend/modules/lambda_api_module/apigw.tf
@@ -120,7 +120,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 
--- resume-frontend/apifetch.js
+++ resume-frontend/apifetch.js
@@ -18,12 +18,12 @@ const countEl = document.getElementById("visitors");
 updateVisitCount();
 
 function updateVisitCount() {
-  fetch('https://zl7hezvfy6.execute-api.us-east-1.amazonaws.com/Prod/visitors')
+  fetch("https://y4qcesst5h.execute-api.us-east-1.amazonaws.com/Prod/visitors")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      document.getElementById("visitors").innerHTML = data['count'];
+      document.getElementById("visitors").innerHTML = data["count"];
     });
 }
